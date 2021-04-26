095e9476f8db17d7d51614923adc1dbea99e079c
Akshat <masterakshata@gmail.com>
2021-04-26 20:11:47 +0530

    fix reducer logic and add seed state

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,10 +1,26 @@
 const initialState = {
-  books: [],
+  books: [
+    {
+      id: 12121,
+      title: 'The book',
+      category: 'Action',
+    },
+    {
+      id: 1211,
+      title: 'The hallows of redux',
+      category: 'Horror',
+    },
+    {
+      id: 111,
+      title: 'The love of redux toolkit',
+      category: 'Romantic',
+    },
+  ],
 };
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_BOOK': {
+    case 'CREATE_BOOK': {
       const book = action.payload;
       return {
         books: [...state.books, book],
