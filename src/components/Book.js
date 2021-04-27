9828c15f82c3fdfc17855737a9a67d67f98529dd
Akshat <masterakshata@gmail.com>
2021-04-27 20:18:56 +0530

    add handleRemoveBook

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,13 @@
 import PropTypes from 'prop-types';
-import { REMOVE_BOOK } from '../actions';
 
 const Book = ({
-  id, title, category, dispatch,
+  id, title, category, handleRemoveBook,
 }) => (
   <tr>
     <td>{id}</td>
     <td>{title}</td>
     <td>{category}</td>
-    <td><button type="button" onClick={() => { dispatch(REMOVE_BOOK({ id, title, category })); }}> Delete </button></td>
+    <td><button type="button" onClick={() => { handleRemoveBook({ id, title, category }); }}> Delete </button></td>
   </tr>
 );
 
@@ -16,7 +15,7 @@ Book.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
-  dispatch: PropTypes.func.isRequired,
+  handleRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
