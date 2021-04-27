ebc395cc2a142622a14fe9bf618ca2985dace2ae
Akshat <masterakshata@gmail.com>
2021-04-27 21:02:36 +0530

    fix CHANGE_FILTER action

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -6,7 +6,7 @@ const filterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CHANGE_FILTER':
       return {
-        filter: action.payload.filter,
+        filter: action.payload,
       };
 
     default:
