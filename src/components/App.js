2d76509afc2b18f2a7debb03087d57a38cad6f2e
Akshat <masterakshata@gmail.com>
2021-04-27 19:18:34 +0530

    add BookForm

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from '../reducers';
 import BookList from '../containers/BookList';
+import BookForm from '../containers/BookForm';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <BookList />
+        <BookForm />
       </div>
     </Provider>
   );
